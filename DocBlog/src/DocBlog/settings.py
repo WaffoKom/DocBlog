5f98d070e8c480fa920d2607699eb29d86b7b2a3
DaniloWaffis <danilowaffis@gmail.com>
2024-11-26 22:55:55 +0100

    Touch one

--- DocBlog/src/DocBlog/settings.py
+++ DocBlog/src/DocBlog/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
+# from django.conf.global_settings import STATICFILES_DIRS
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -118,7 +120,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR, "DocBlog/static")
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
