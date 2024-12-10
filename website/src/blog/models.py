4a7b816a211eb444a61cee8bf68c7436a3dcc9db
DaniloWaffis <danilowaffis@gmail.com>
2024-12-10 15:00:23 +0100

    create user admin

--- website/src/blog/models.py
+++ website/src/blog/models.py
@@ -1,11 +1,31 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import ForeignKey, SET_NULL
+from django.template.defaultfilters import title
+from django.utils.text import slugify
+
 
 # Create your models here.
 
+class Categories(models.Model):
+    name =models.CharField(max_length=30)
+    slug =models.SlugField()
+
 class BlogPost(models.Model):
+    author =models.ForeignKey(User, on_delete=SET_NULL, null=True)
     title =models.CharField(max_length=150)
     slug =models.SlugField()
     publish =models.BooleanField(default=False)
-    date =models.DateField(blank=True)
+    date =models.DateField(blank=True, null=True)
     content =models.TextField()
     description =models.TextField()
+
+    # def publish_string(self):
+    #     if self.publish:
+    #         return "L'article a ete publier"
+    #     return "L'article n'as pas ete publier"
+
+    def save(self,*args, **kwargs ):
+        if not self.slug or self.slug=="":
+            self.slug =slugify(self.title)
+        super().save(*args,**kwargs)
